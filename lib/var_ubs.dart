9f3e9c694cd2f5b08ffa131b7f0c79400a6e297a
juliavitoriav <92683917+juliavitoriav@users.noreply.github.com>
2022-09-06 23:08:28 -0300

    Add files via upload

---
+++ lib/var_ubs.dart
@@ -0,0 +1,25 @@
+class VarUbs {
+  final String nome;
+  final String end;
+  final String bairro;
+  final String cep;
+  final String hora;
+  final String tel;
+  final String urlimage;
+  
+  
+
+
+
+  VarUbs({
+    required this.nome,
+    required this.end,
+    required this.bairro,
+    required this.cep,
+    required this.hora,
+    required this.tel,
+    required this.urlimage,
+    
+
+  });
+}
---
+++ windows/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,18 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "C:\\src\\flutter\\flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "C:\\Users\\julia\\OneDrive\\Documentos\\flutter\\ubs-arasaude\\ubs_arasaude" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "PROJECT_DIR=C:\\Users\\julia\\OneDrive\\Documentos\\flutter\\ubs-arasaude\\ubs_arasaude"
+  "FLUTTER_ROOT=C:\\src\\flutter\\flutter"
+  "FLUTTER_EPHEMERAL_DIR=C:\\Users\\julia\\OneDrive\\Documentos\\flutter\\ubs-arasaude\\ubs_arasaude\\windows\\flutter\\ephemeral"
+  "PROJECT_DIR=C:\\Users\\julia\\OneDrive\\Documentos\\flutter\\ubs-arasaude\\ubs_arasaude"
+  "FLUTTER_TARGET=C:\\Users\\julia\\OneDrive\\Documentos\\flutter\\ubs-arasaude\\ubs_arasaude\\lib\\main.dart"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=C:\\Users\\julia\\OneDrive\\Documentos\\flutter\\ubs-arasaude\\ubs_arasaude\\.dart_tool\\package_config.json"
+)
